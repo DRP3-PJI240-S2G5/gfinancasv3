0c7e0845e7eead18643c6318cae519b9eb27dbcc
Fernando <sax@gmail.com>
2024-11-16 19:01:48 -0300

    add devcontainer

--- gfinancas4/core/admin.py
+++ gfinancas4/core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Departamento
+from .models import Departamento, Subordinacao
 
 
 class DepartamentoAdmin(admin.ModelAdmin):
@@ -10,4 +10,13 @@ class DepartamentoAdmin(admin.ModelAdmin):
         (None, {"fields": ( "nome", "description", "tipoEntidade", "responsavelId", "done")}),
     )
 
+class SubordinacaoAdmin(admin.ModelAdmin):
+    list_display = ("IdDepartamentoA", "IdDepartamentoB", "dataSubordinacao", "Observacao")
+    list_filter = ("dataSubordinacao",)
+    search_fields = ("IdDepartamentoA__description", "IdDepartamentoB__description")
+    fieldsets = (
+        (None, {"fields": ("IdDepartamentoA", "IdDepartamentoB", "dataSubordinacao", "Observacao")}),
+    )
+
 admin.site.register(Departamento, DepartamentoAdmin)
+admin.site.register(Subordinacao, SubordinacaoAdmin)
--- gfinancas4/core/models.py
+++ gfinancas4/core/models.py
@@ -22,3 +22,28 @@ class Departamento(models.Model):
             "responsavelId": self.responsavelId.id if self.responsavelId else None,
             "done": self.done,
         }
+
+class Subordinacao(models.Model):
+    IdDepartamentoA = models.ForeignKey(
+        Departamento, 
+        on_delete=models.CASCADE, 
+        related_name="departamentos_superiores"
+    )
+    IdDepartamentoB = models.ForeignKey(
+        Departamento, 
+        on_delete=models.CASCADE, 
+        related_name="departamentos_subordinados"
+    )
+    dataSubordinacao = models.DateTimeField(auto_now_add=True)
+    Observacao = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.IdDepartamentoB} subordinado a {self.IdDepartamentoA} desde {self.dataSubordinacao}"
+
+    def to_dict_json(self):
+        return {
+            "IdDepartamentoA": self.IdDepartamentoA.id if self.IdDepartamentoA else None,
+            "IdDepartamentoB": self.IdDepartamentoB.id if self.IdDepartamentoB else None,
+            "dataSubordinacao": self.dataSubordinacao,
+            "Observacao": self.Observacao,
+        }
